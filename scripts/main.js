4f05afb77115043b3321ce2d482972c83d345377
Tech-FZ <86562548+Tech-FZ@users.noreply.github.com>
2022-06-10 12:23:41 +0200

    Added a tutorial

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 let vidpage = document.getElementsByClassName("vidpage");
 let emuguiAboutImg = document.getElementById("emuguiAboutImg");
-
+let winNT4MipsQemu = document.getElementById("winNT4MipsQemu");
 
 let devWidth = window.screen.availWidth;
 let devHeight = window.screen.availHeight;
@@ -15,24 +15,69 @@ if (devWidth < 330 || devHeight < 125) {
     vidWidth = "240";
     vidHeight = "135";
 
-    emuguiAboutImg.width = "302";
-    emuguiAboutImg.height = "249";
+    try {
+        emuguiAboutImg.width = "302";
+        emuguiAboutImg.height = "249";
+    }
+    
+    catch {
+        console.log("EmuGUI About IMG doesn't exist.");
+    }
+
+    try {
+        winNT4MipsQemu.width = "274";
+        winNT4MipsQemu.height = "231";
+    }
+
+    catch {
+        console.log("WinNT4 MIPS QEMU IMG doesn't exist.");
+    }
 }
 
 else if (devWidth < 800 || devHeight < 400) {
     vidWidth = "320";
     vidHeight = "180";
 
-    emuguiAboutImg.width = "302";
-    emuguiAboutImg.height = "249";
+    try {
+        emuguiAboutImg.width = "302";
+        emuguiAboutImg.height = "249";
+    }
+    
+    catch {
+        console.log("EmuGUI About IMG doesn't exist.");
+    }
+
+    try {
+        winNT4MipsQemu.width = "274";
+        winNT4MipsQemu.height = "231";
+    }
+
+    catch {
+        console.log("WinNT4 MIPS QEMU IMG doesn't exist.");
+    }
 }
 
 else {
     vidWidth = "640";
     vidHeight = "360";
 
-    emuguiAboutImg.width = "602";
-    emuguiAboutImg.height = "497";
+    try {
+        emuguiAboutImg.width = "602";
+        emuguiAboutImg.height = "497";
+    }
+    
+    catch {
+        console.log("EmuGUI About IMG doesn't exist.");
+    }
+
+    try {
+        winNT4MipsQemu.width = "547";
+        winNT4MipsQemu.height = "462";
+    }
+
+    catch {
+        console.log("WinNT4 MIPS QEMU IMG doesn't exist.");
+    }
 }
 
 let i = 0;
